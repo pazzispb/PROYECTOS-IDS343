ec7d45d5cd848a8f3534e490c8f91dc33cbeae16
Pazzis Paulino <1103790@est.intec.edu.do>
2022-10-02 20:48:27 -0400

    Colores agregados

--- Torres de Hanoi/TorresDeHanoi.cpp
+++ Torres de Hanoi/TorresDeHanoi.cpp
@@ -36,7 +36,7 @@ unsigned long long contador = 0; //Contador para la cantidad de pasos del algori
 void MoverDisco(int disco, char torreA, char torreB, char torreC); //torreA -> origen, torreB -> auxiliar, torreC -> destino
 void EntradaDisco(int& disco); //valida que el usuario introduzca 3 discos o mas
 void ImprimirTitulo(); //imprime el titulo del programa
-
+void ImprimirMovimiento(char torreOrigen, char torreDestino); //Impresion del movimiento
 //Función principal
 int main()
 {
@@ -73,14 +73,37 @@ void MoverDisco(int disco, char torreA, char torreB, char torreC){
 	
 	if(disco == 1){ //si se solo se esta jugando con un disco, se movera directamente de la torre A (origen) a la torre C (destino)
 		contador++; //se suma el paso
-		cout<< contador <<". Mover disco de la torre " << torreA << " hacia la torre " << torreC << endl;
+		ImprimirMovimiento(torreA, torreC);
 	}
 	else{ // si tenemos mas de un disco
 		MoverDisco(disco - 1, torreA, torreC, torreB); 
 		// moveremos los primeros n-1 discos (todos menos el ultimo) de la torre A (origen) a la torre B (auxiliar)
 		contador++; //se suma el paso
-		cout << contador <<". Mover disco de la torre "<< torreA <<" hacia la torre "<< torreC << endl; //moveremos el unico disco de la torre A (origen) a la torre C (destino) 
+		ImprimirMovimiento(torreA, torreC); //moveremos el unico disco de la torre A (origen) a la torre C (destino) 
 		MoverDisco(disco - 1, torreB, torreA, torreC); // Los discos contenidos en la torre B (auxiliar) se mueven a la torre C (destino)
 	}
 }
-
+//torre A -> rojo 4    torre B -> azul 9      torre C -> 6 amarilla
+void ImprimirMovimiento(char torreOrigen, char torreDestino){
+	int colorOrigen = 0, colorDestino = 0; //colores para las torres
+	switch(torreOrigen){ //cambia el color de la torre de origen
+		case 'A': colorOrigen = 4; break;
+		case 'B': colorOrigen = 9; break;
+		case 'C': colorOrigen = 6; break;
+	}
+	switch(torreDestino){ //cambia el color de la torre destino
+		case 'A': colorDestino = 4; break;
+		case 'B': colorDestino = 9; break;
+		case 'C': colorDestino = 6; break;
+	}
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE); //toma control de la consola
+	SetConsoleTextAttribute(h, 7); //Blanco
+	cout<< contador <<". Mover disco de la torre ";
+	SetConsoleTextAttribute(h, colorOrigen); //cambiamos el color de salida de la torre de origen
+	cout << torreOrigen;
+	SetConsoleTextAttribute(h, 7); //Blanco
+	cout << " hacia la torre ";
+	SetConsoleTextAttribute(h, colorDestino); //cambiamos el color de salida de la torre de destino
+	cout << torreDestino << endl << endl;
+	SetConsoleTextAttribute(h, 7); //Blanco
+}
